3ef7d6dc45c2ba56060678428a0c17bad50478fd
Nicolas Souveton <souvetonnicolas@gmail.com>
2026-08-21 18:01:14 +0200

    Add English plural forms

---
+++ translations/harbour-nami-en.ts
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>CoverPage</name>
+    <message numerus="yes">
+        <source>%n people</source>
+        <translation>
+            <numerusform>%n person</numerusform>
+            <numerusform>%n people</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n photos</source>
+        <translation>
+            <numerusform>%n photo</numerusform>
+            <numerusform>%n photos</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>EventsPage</name>
+    <message numerus="yes">
+        <source>Add %n day(s)</source>
+        <translation>
+            <numerusform>Add %n day</numerusform>
+            <numerusform>Add %n days</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>Group %n day(s)</source>
+        <translation>
+            <numerusform>Group %n day</numerusform>
+            <numerusform>Group %n days</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>PersonDetailPage</name>
+    <message numerus="yes">
+        <source>%n match(es) to confirm</source>
+        <translation>
+            <numerusform>%n match to confirm</numerusform>
+            <numerusform>%n matches to confirm</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>PhotoSelectionBar</name>
+    <message numerus="yes">
+        <source>%n selected</source>
+        <translation>
+            <numerusform>%n selected</numerusform>
+            <numerusform>%n selected</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>ScanResultsPage</name>
+    <message numerus="yes">
+        <source>%n faces need to be identified</source>
+        <translation>
+            <numerusform>%n face needs to be identified</numerusform>
+            <numerusform>%n faces need to be identified</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>ScanningPage</name>
+    <message numerus="yes">
+        <source>Found %n face(s)</source>
+        <translation>
+            <numerusform>Found %n face</numerusform>
+            <numerusform>Found %n faces</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message numerus="yes">
+        <source>Last backup: %n day(s) ago</source>
+        <translation>
+            <numerusform>Last backup: %n day ago</numerusform>
+            <numerusform>Last backup: %n days ago</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
